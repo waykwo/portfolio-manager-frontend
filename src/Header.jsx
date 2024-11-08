fdf07529e7023bddacb3346035825c5bf052acd3
waykwo <72991933+waykwo@users.noreply.github.com>
2024-11-08 14:21:56 -0500

    PortfolioPage, PortfolioIndex, Login

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,11 +1,16 @@
 // import { Link } from "react-router-dom";
+import { LoginPage } from "./LoginPage";
 
 export function Header() {
   return (
     <header>
       <nav>
-        {/* <Link to="#">Home</Link> | <Link to="#">Link</Link> */}
-        <a href="#">Home</a> | <a href="#">Link</a>
+        {/* <Link to="#">Home</Link> | <Link to="#">Link</Link> | <Link to="./LoginPage"></Link> */}
+        <a href="#">Home</a> |&nbsp;
+        <a href="#">Link</a> |&nbsp;
+        <a href="./LoginPage">Login</a> |&nbsp;
+        <a href="./SignupPage">Signup</a> |&nbsp;
+        <a href="./LogoutLink">Logout</a> |&nbsp;
       </nav>
     </header>
   )
--- src/PortfolioPage.jsx
+++ src/PortfolioPage.jsx
@@ -1,7 +1,24 @@
+import { PortfolioIndex } from "./PortfolioIndex"
+import axios from "axios";
+import { useState, useEffect } from "react";
+
 export function PortfolioPage() {
+  const [assets, setAssets] = useState([]);
+
+  const handleIndex = () => {
+    console.log("handleIndex");
+    axios.get("http://localhost:3000/transactions.json").then((response) => {
+      console.log(response.data);
+      setAssets(response.data);
+    });
+  };
+
+  useEffect(handleIndex, []);
+
   return (
     <main>
       <h1>Welcome to React!</h1>
+      <PortfolioIndex assets={assets}/>
     </main>
-  )
+  );
 }
