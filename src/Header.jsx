e442a581349fc60e3ba240691e96dc1feb0a27d0
waykwo <72991933+waykwo@users.noreply.github.com>
2024-11-08 13:18:58 -0500

    Basic setup

---
+++ src/Header.jsx
@@ -0,0 +1,12 @@
+// import { Link } from "react-router-dom";
+
+export function Header() {
+  return (
+    <header>
+      <nav>
+        {/* <Link to="#">Home</Link> | <Link to="#">Link</Link> */}
+        <a href="#">Home</a> | <a href="#">Link</a>
+      </nav>
+    </header>
+  )
+}
