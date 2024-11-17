e3466374e9a5683f332a8486052a23e1650c2d0d
waykwo <72991933+waykwo@users.noreply.github.com>
2024-11-17 14:29:40 -0500

    Code cleanup

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,5 +1,4 @@
-// import { Link } from "react-router-dom";
-// import { LoginPage } from "./LoginPage";
+
 import { LogoutLink } from "./LogoutLink";
 import { useEffect, useState } from "react";
 import axios from "axios";
@@ -18,9 +17,8 @@ export function Header() {
   if (localStorage.jwt === undefined) {
     authenticationLinks = (
       <div>
-        {/* <a href="./LoginPage">Login</a> |&nbsp; */}
         <a href="./SignupPage">Signup</a>
-        {/* <Link to="/signup">Signup</Link> |&nbsp; */}
+
       </div>
     )
   } else {
@@ -33,18 +31,6 @@ export function Header() {
   return (
     <header>
       <nav>
-        {/* <Link to="/">Home</Link> |&nbsp;
-        <Link to="/">Link</Link> |&nbsp;
-        <Link to="./LoginPage"></Link> */}
-
-        {/* <a href="#">Portfolio</a> */}
-        {/* &nbsp;|&nbsp; <a href="#">Equities</a> */}
-        {/* &nbsp;|&nbsp; <a href="#">Fixed Income</a> */}
-        {/* &nbsp;|&nbsp; <a href="#">Commodities</a> */}
-        {/* &nbsp;|&nbsp; <a href="#">Gold</a> */}
-        {/* &nbsp;|&nbsp; <a href="./LoginPage">Login</a> */}
-        {/* &nbsp;|&nbsp; <a href="./SignupPage">Signup</a> */}
-        {/* &nbsp;|&nbsp; <a href="./LogoutLink">Logout</a> */}
         <div className="float-left font-raleway text-xl">
           {user}
         </div>
